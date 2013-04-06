c882cbb3c419d04eac1cd8f0ff802c28dfb49790
DMZDesolace <dmzdesolace@gmail.com>
2013-04-06 16:34:58 -0300

    Added createTeacher(E e) and validate(E e)

--- Business/manageTeacher.java
+++ Business/manageTeacher.java
@@ -1,5 +1,25 @@
 package business;
 
-public class manageTeacher {
+import data_access.*;
+import model.*;
 
+public class manageTeacher {
+  int teacherID;
+	int valid=0;
+	
+	Access dataAccess = new Access();
+	Insert newInsert = new Insert();
+	Retrieval retrieve = new Retrieval();
+	
+	public int validate(Entity entity)
+	{
+		teacherID = dataAccess.LogUser(entity, "authenticate");
+		return teacherID;
+	}
+	public int createTeacher(Entity entity)
+	{
+		valid = newInsert.createTeacher(entity);
+		return valid;
+	}
+	
 }
